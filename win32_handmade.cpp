34d6136818dde5644efe08d22b33f793aa028725
Ravolaparthi Sampath Sukesh <rsampaths16@gmail.com>
2025-10-07 03:14:41 +0530

    feat: open a basic win32 window
    Followed the Video: Handmade Hero Day 002 - Opening a Win32 Window
    Registerd & created a window, while listening to basic message loop
    
    REFERENCE: https://youtu.be/4ROiWonnWGk?si=1FstKwwFt7PO9dg2

--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -1,7 +1,114 @@
 #include <windows.h>
 
-int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline,
-                     int cmdshow) {
-  return MessageBox(NULL, "A message for the Handmade Hero Game",
-                    "Handmade Hero", 0);
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam,
+                                    LPARAM LParam) {
+  LRESULT Result = 0;
+
+  switch (Message) {
+  case WM_SIZE: {
+    OutputDebugStringA("WM_SIZE\n");
+    break;
+  }
+  case WM_DESTROY: {
+    OutputDebugStringA("WM_DESTROY\n");
+    break;
+  }
+  case WM_CLOSE: {
+    OutputDebugStringA("WM_CLOSE\n");
+    break;
+  }
+  case WM_ACTIVATEAPP: {
+    OutputDebugStringA("WM_ACTIVATEAPP\n");
+    break;
+  }
+  case WM_PAINT: {
+    OutputDebugStringA("WM_PAINT\n");
+    PAINTSTRUCT Paint;
+    HDC DeviceContext = BeginPaint(Window, &Paint);
+    LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+    LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
+    LONG X = Paint.rcPaint.left;
+    LONG Y = Paint.rcPaint.top;
+    static DWORD ColorCode = WHITENESS;
+    if (ColorCode == WHITENESS) {
+      ColorCode = BLACKNESS;
+    } else {
+      ColorCode = WHITENESS;
+    }
+    PatBlt(DeviceContext, X, Y, Width, Height, ColorCode);
+    EndPaint(Window, &Paint);
+    break;
+  }
+  default: {
+    // OutputDebugStringA("default\n");
+    Result = DefWindowProc(Window, Message, WParam, LParam);
+    break;
+  }
+  }
+
+  return Result;
+}
+
+WNDCLASSEXA ConstructMainWindowClass(HINSTANCE Instance) {
+  WNDCLASSEXA WindowClass = {};
+  WindowClass.cbSize = sizeof(WNDCLASSEXA);
+  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+  // TODO: configure hIcon
+  // WindowClass.hIcon = ;
+
+  return WindowClass;
+}
+
+HWND RegisterAndCreateWindow(HINSTANCE Instance, PWNDCLASSEXA WindowClassPtr) {
+  ATOM WindowClassAtom = RegisterClassExA(WindowClassPtr);
+  if (WindowClassAtom != 0) {
+    HWND Window = CreateWindowExA(0, "HandmadeHeroWindowClass", "HandemadeHero",
+                                  WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                                  CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                                  CW_USEDEFAULT, 0, 0, Instance, 0);
+    if (Window == NULL) {
+      // TODO: Handle / log window creation failures
+      OutputDebugStringA("Failure in window creation\n");
+
+      return NULL;
+    }
+
+    return Window;
+  } else {
+    // TODO: Handle / log window registration failures
+    OutputDebugStringA("Failure in window registration\n");
+
+    return NULL;
+  }
+}
+
+void MessageLoop() {
+  MSG Message;
+  while (true) {
+    BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+    if (MessageResult > 0) {
+      TranslateMessage(&Message);
+      DispatchMessage(&Message);
+    } else {
+      // TODO: Handle / log message retrival failures
+      OutputDebugStringA("Failure in message retrival\n");
+      break;
+    }
+  }
+}
+
+int APIENTRY WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR Cmdline,
+                     int ShowCode) {
+  WNDCLASSEXA WindowClass = ConstructMainWindowClass(Instance);
+  HWND Window = RegisterAndCreateWindow(Instance, &WindowClass);
+
+  if (Window != NULL) {
+    MessageLoop();
+  }
+
+  return 0;
 }
