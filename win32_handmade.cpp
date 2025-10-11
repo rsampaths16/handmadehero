2876c58eb97791b189b6824fcaf47cfe790e5975
Ravolaparthi Sampath Sukesh <rsampaths16@gmail.com>
2025-10-11 13:02:53 +0530

    fix: Send appropriate response code in xbox controller stub function
    0 means ERROR_SUCCESS, which is something we want to avoid sending in
    stub functions.
    
    Sending ERROR_DEVICE_NOT_CONNECTED as the appropriate response.

--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -36,7 +36,7 @@ struct win32_window_dimension {
 #define X_INPUT_GET_STATE(name)                                                \
   DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
 typedef X_INPUT_GET_STATE(x_input_get_state);
-X_INPUT_GET_STATE(XInputGetStateStub) { return 0; }
+X_INPUT_GET_STATE(XInputGetStateStub) { return ERROR_DEVICE_NOT_CONNECTED; }
 global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
 #define XInputGetState XInputGetState_
 
@@ -44,7 +44,7 @@ global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
 #define X_INPUT_SET_STATE(name)                                                \
   DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
 typedef X_INPUT_SET_STATE(x_input_set_state);
-X_INPUT_SET_STATE(XInputSetStateStub) { return 0; }
+X_INPUT_SET_STATE(XInputSetStateStub) { return ERROR_DEVICE_NOT_CONNECTED; }
 global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
 #define XInputSetState XInputSetState_
 
