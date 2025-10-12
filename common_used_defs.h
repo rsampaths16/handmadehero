82254f5c8ae9cde84044adecdcd5ad369e2a80ea
Ravolaparthi Sampath Sukesh <rsampaths16@gmail.com>
2025-10-12 22:09:30 +0530

    refactor: extract common used defs into a header

---
+++ common_used_defs.h
@@ -0,0 +1,24 @@
+#ifndef COMMON_USED_DEF_H
+#define COMMON_USED_DEF_H
+
+#include <stdint.h>
+
+#define internal static
+#define local_variable static
+#define global_variable static
+#define PI32 3.14159265358979323846f
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef float real32;
+typedef double real64;
+
+#endif
--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -7,23 +7,7 @@
 #include <windows.h>
 #include <xinput.h>
 
-#define internal static
-#define local_variable static
-#define global_variable static
-#define PI32 3.14159265358979323846f
-
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-typedef float real32;
-typedef double real64;
+#include "common_used_defs.h"
 
 struct win32_offscreen_buffer {
   /*
