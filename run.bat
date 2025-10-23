9a4db1dfd13083d6a3a7241bef2f5033d9ce3792
Ravolaparthi Sampath Sukesh <rsampaths16@gmail.com>
2025-10-23 16:53:11 +0530

    chore: run the executable from inside the Debug folder

--- run.bat
+++ run.bat
@@ -20,12 +20,18 @@ if %ERRORLEVEL% NEQ 0 (
     exit /b 1
 )
 
+rem Change current working directory to Debug
+cd build\Debug
+
 rem Start the application
-start "" build\Debug\win32_handmade.exe
+start "" win32_handmade.exe
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to start win32_handmade.exe.
     exit /b 1
 )
+
+rem Go back to old directory
+cd ../..
 
 rem Exit the script
 exit /b 0
