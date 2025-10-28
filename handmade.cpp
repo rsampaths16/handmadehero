7d1c287213c8b170d5e0531c1cb198f3cb5bb9db
Ravolaparthi Sampath Sukesh <rsampaths16@gmail.com>
2025-10-28 23:39:05 +0530

    chore: enable audio again

--- handmade.cpp
+++ handmade.cpp
@@ -10,7 +10,7 @@ internal void GameSoundOutput(game_state *GameState,
   int16 *SampleOut = SoundBuffer->Samples;
   for (int SampleIndex = 0; SampleIndex < SoundBuffer->SampleCount;
        SampleIndex++) {
-#if 0
+#if 1
     real32 SineValue = sinf(GameState->tSine);
     int16 SampleValue = (int16)(SineValue * ToneVolume);
 #else
